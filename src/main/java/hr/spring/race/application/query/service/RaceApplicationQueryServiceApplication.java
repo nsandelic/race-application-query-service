a2c14656a20245c21a0b5ef51a8ee0ab30d97b40
Nikola Sandelic <sandelic.nikola@gmail.com>
2024-12-01 17:41:57 +0100

    init

---
+++ src/main/java/hr/spring/race/application/query/service/RaceApplicationQueryServiceApplication.java
@@ -0,0 +1,13 @@
+package hr.spring.race.application.query.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaceApplicationQueryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RaceApplicationQueryServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hr/spring/race/application/query/service/RaceApplicationQueryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.spring.race.application.query.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaceApplicationQueryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
